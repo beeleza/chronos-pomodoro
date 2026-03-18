2ccf4877bf241d2c62faf0e4abaa3334682a79ce
beeleza <higorbeleza.dev@gmail.com>
2026-03-18 16:02:18 -0300

    feat(components): implement CountDown timer and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import "./styles/theme.css";
 import "./styles/global.css";
 import { Container } from "./components/Container";
-import Heading from "./components/Heading";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
+import { CountDown } from "./components/CountDown";
 
 function App() {
   return (
@@ -13,6 +13,9 @@ function App() {
       </Container>
       <Container>
         <Menu />
+      </Container>
+      <Container>
+        <CountDown />
       </Container>
     </>
   );
