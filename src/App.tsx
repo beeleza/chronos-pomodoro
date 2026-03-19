1341de0ec0d1108effb0da2ae01593bc89b80e6f
beeleza <higorbeleza.dev@gmail.com>
2026-03-19 03:38:44 -0300

    feat: creating the Home page, a template for all other pages and footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,10 @@
+import Home from "./pages/Home";
+
 import "./styles/theme.css";
 import "./styles/global.css";
-import { Container } from "./components/Container";
-import { Logo } from "./components/Logo";
-import { Menu } from "./components/Menu";
-import { CountDown } from "./components/CountDown";
-import { DefaultInput } from "./components/DefaultInput";
-import { Cycles } from "./components/Cycles";
-import { DefaultButton } from "./components/DefaultButton";
-import { PlayCircleIcon, StopCircleIcon } from "lucide-react";
 
 function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-      <Container>
-        <Menu />
-      </Container>
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <form className='form' action=''>
-          <div className='formRow'>
-            <DefaultInput
-              labelText='task'
-              id='meuInput'
-              type='text'
-              placeholder='Digite algo'
-            />
-          </div>
-
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className='formRow'>
-            <Cycles />
-          </div>
-
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon />} color='green' />
-            <DefaultButton icon={<StopCircleIcon />} color='red' />
-          </div>
-        </form>
-      </Container>
-    </>
-  );
+  return <Home />;
 }
 
 export default App;
