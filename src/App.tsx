7a58ca10fb4165982ec92e5c2ea8dc29c1b2f433
beeleza <higorbeleza.dev@gmail.com>
2026-03-29 14:51:44 -0300

    feat: global state app with useState

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,26 @@ import Home from "./pages/Home";
 
 import "./styles/theme.css";
 import "./styles/global.css";
+import type { TaskStateModel } from "./models/TaskStateModel";
+import { useState } from "react";
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: "00:00",
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15
+  }
+}
 
 function App() {
-  return <Home />;
+  const [state, setState] = useState<TaskStateModel>(initialState);
+
+  return <Home state={state} setState={setState} />;
 }
 
 export default App;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,17 @@
 import { Container } from "../../components/Container";
 import { CountDown } from "../../components/CountDown";
 import { MainForm } from "../../components/MainForm";
+import type { TaskStateModel } from "../../models/TaskStateModel";
 import MainTemplate from "../../templates/MainTemplate";
 
-function Home() {
+type HomeProps = {
+  state: TaskStateModel;
+  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+};
+
+function Home({ state }: HomeProps) {
+  console.log(state);
+
   return (
     <MainTemplate>
       <Container>
